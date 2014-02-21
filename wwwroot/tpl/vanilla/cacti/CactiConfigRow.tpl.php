44b56d52e3b6ccc6e55be19def65ca964c3654f3
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-02-21 15:33:46 +0100

    Forgot to add new files last time.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

---
+++ wwwroot/tpl/vanilla/cacti/CactiConfigRow.tpl.php
@@ -0,0 +1,11 @@
+<?php if (defined("RS_TPL")) {?>
+		<tr align=left valign=top>
+			<td><?php $this->getH("NiftyString", "%%BaseUrl"); ?></td>
+			<td><?php $this->UserName; ?></td>
+			<td class=tdright><?php $this->NumGraphs; ?></td>
+		</tr>
+<?php } else { ?>
+Don't use this page directly, it's supposed <br />
+to get loaded within the main page. <br />
+Return to the index. <br />
+<?php }?>
