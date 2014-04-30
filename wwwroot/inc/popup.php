3b1447c46f2d2c7aec665c974f1cec254d597b79
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-04-30 16:49:43 +0200

    Fixing popups.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/inc/popup.php
+++ wwwroot/inc/popup.php
@@ -586,6 +586,7 @@ function renderPopupHTML()
 			throw new InvalidRequestArgException ('helper', $_REQUEST['helper']);
 	}
 	
+	ob_end_clean();
 	echo 'running popup';
 	$tplm->run(true);
 	echo 'run succesfull';
