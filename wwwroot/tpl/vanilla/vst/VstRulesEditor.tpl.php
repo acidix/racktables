000276939bbafdd6381499f39c6bd93e8e728a3c
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-04-30 15:44:26 +0200

    Still fixing VST
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/vanilla/vst/VstRulesEditor.tpl.php
+++ wwwroot/tpl/vanilla/vst/VstRulesEditor.tpl.php
@@ -4,11 +4,11 @@
 		$this->addJS('js/vst_editor.js');
 	
 		$row_html  = "<td><a href='#' class='vst-del-rule'> <img width='16' height='16' border='0' title='delete rule' src='?module=chrome&uri=pix/tango-list-remove.png'> </a></td>"; 
-		$row_html .= "<td><input type=text name=rule_no value='%s' size=3></td>";
-		$row_html .= "<td><input type=text name=port_pcre value='%s'></td>";
+		$row_html .= "<td><input type=text name=rule_no value='' size=3></td>";
+		$row_html .= "<td><input type=text name=port_pcre value=''></td>";
 		$row_html .= '<td>' . $this->AccessSelect . '</td>';
-		$row_html .= "<td><input type=text name=wrt_vlans value='%s'></td>";
-		$row_html .= "<td><input type=text name=description value='%s'></td>";
+		$row_html .= "<td><input type=text name=wrt_vlans value=''></td>";
+		$row_html .= "<td><input type=text name=description value=''></td>";
 		$row_html .= "<td><a href='#' class='vst-add-rule'> <img width='16' height='16' border='0' title='Duplicate rule' src='?module=chrome&uri=pix/tango-list-add.png'> </a></td>"; 
 		$this->addJS("var new_vst_row = \"" . addslashes($row_html) . "\";", TRUE);
 
@@ -21,7 +21,7 @@
 	<input type=hidden name="mutex_rev" value=" <?php $this->VstMutexRef; ?> ">
 	<table cellspacing=0 cellpadding=5 align=center class=widetable>
 		<tr>
-			<td> <?php $this->Getselect; ?> </td>
+			<td> <?php $this->AccesSelectClone; ?> </td>
 			<td> <?php $this->getH('PrintImageHref', array('COPY', 'copy from selected', TRUE)); ?> </td>
 		</tr>
 	</table>
@@ -38,7 +38,7 @@
 				<td><a href="#" class="vst-del-rule"> <img width="16" height="16" border="0" title="delete rule" src="?module=chrome&uri=pix/tango-list-remove.png"> </a></td>
 				<td><input type=text name=rule_no value="<?php $this->RuleNo; ?>" size=3></td>
 				<td><input type=text name=port_pcre value="<?php $this->PortPCRE; ?>"></td>
-				<td><?php $this->AccessSelect ?></td>
+				<td><?php $this->AccessSelectSingle ?></td>
 				<td><input type=text name=wrt_vlans value="<?php $this->WRTVlans; ?>"></td>
 				<td><input type=text name=description value="<?php $this->Description; ?>"></td>
 				<td><a href="#" class="vst-add-rule"> <img width="16" height="16" border="0" title="Duplicate rule" src="?module=chrome&uri=pix/tango-list-add.png"> </a></td>
@@ -46,7 +46,7 @@
 		<?php $this->endLoop(); ?>
 	</table>
 	<input type=hidden name="template_json">	
-	<input type=hidden name="mutex_rev" value=" <?php $this->MutexRev; ?> ">
+	<input type=hidden name="mutex_rev" value="<?php $this->MutexRev; ?>">
 	<center> <?php $this->getH('PrintImageHref', array('SAVE', 'Save template', TRUE)); ?> </center>
 	</form>
 	<?php $this->VstRules; ?>
