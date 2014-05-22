53cacee3eb7deb93adebf5bfc51727784aa8fcbf
Jakob Frick <jakob@aussenwasser.de>
2014-05-22 22:32:04 +0200

    test for vm

--- wwwroot/tpl/vanilla/slb2_interface/GetPopupSLBConfig.tpl.php
+++ wwwroot/tpl/vanilla/slb2_interface/GetPopupSLBConfig.tpl.php
@@ -1,15 +1,10 @@
 <?php if (defined("RS_TPL")) {?>
 	<div class="slbconf-btn">…</div>
 	<div class="slbconf popup-box">
-	<?php if ($this->is("do_vs", true)) { ?>
-		<h1>VS config:</h1>
-		<?php $this->row_vsconfig ?> 
-	<?php } ?> 
-
-	<?php if ($this->is("do_rs", true)) { ?>
-		<h1>RS config:</h1>
-		<?php $this->row_rsconfig ?> 
-	<?php } ?> 
+	<?php if ($this->is("row_vsconfig", true)) { ?>
+		<h1>VS config:</h1><?php $this->row_vsconfig ?><?php } ?>
+	<?php if ($this->is("row_rsconfig", true)) { ?>
+		<h1>RS config:</h1><?php $this->row_rsconfig ?><?php } ?> 
 	</div>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
