23893e404e5e20983a821084c80f3a6b693bc80c
Jakob Frick <jakob@aussenwasser.de>
2014-06-05 18:48:48 +0200

    Fixed vanilla quicklinks

--- wwwroot/tpl/vanilla/Quicklinks.tpl.php
+++ wwwroot/tpl/vanilla/Quicklinks.tpl.php
@@ -1,7 +1,7 @@
 <?php if (defined("RS_TPL")) {?>
 	<ul class="qlinks">
 		<?php $this->startLoop("Quicklinks_Data"); ?>		
-			<li><a href=<?php $this->get("href")?>><?php $this->get("title");?></a></li>
+			<li><a href=<?php $this->get("href")?>><?php $this->get("title");?> </a></li>
 		<?php $this->endLoop(); ?>
 	</ul>
 <?php } else { ?>
