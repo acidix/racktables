e4b092990ccd43d60fde133bcc93c781487130fc
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-08-19 17:29:13 +0200

    Moved js includes

--- wwwroot/tpl/bootstrap/index.tpl.php
+++ wwwroot/tpl/bootstrap/index.tpl.php
@@ -21,6 +21,15 @@
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
 		
+		<!-- jQuery 2.0.2 -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="./js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- AdminLTE App -->
+        <script src="./js/AdminLTE/app.js" type="text/javascript"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="./js/AdminLTE/demo.js" type="text/javascript"></script>
+        
 		<?php $this->Header ?>
     </head>
     <body class="skin-blue">
@@ -147,13 +156,6 @@
         </div><!-- ./wrapper -->
 
 
-        <!-- jQuery 2.0.2 -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <!-- Bootstrap -->
-        <script src="./js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- AdminLTE App -->
-        <script src="./js/AdminLTE/app.js" type="text/javascript"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="./js/AdminLTE/demo.js" type="text/javascript"></script>
+        
     </body>
 </html>
