a014eac42ce7dd55ebc7fcafb1371d5d3fe27137
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-08-22 18:07:27 +0200

    Modified wrong TagRoller.tpl.php. Copied.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/row/TagRoller.tpl.php
+++ wwwroot/tpl/bootstrap/row/TagRoller.tpl.php
@@ -1,24 +1,31 @@
 <?php if (defined("RS_TPL")) {?>
 	<?php $this->getH('PrintOpFormIntro',array('rollTags', array ('realsum' => $this->_sum))); ?>
-	<table border=1 align=center>
-	<tr>
-		<td colspan=2>
-			This special tool allows assigning tags to physical contents (racks <strong>and all contained objects</strong>) of the current ack row.<br>
-			The tag(s) selected below will be appended to already assigned tag(s) of each particular entity. 
-		</td>
-	</tr>
-	<tr>
-		<th>Tags</th>
-		<td><?php $this->Tags; ?></td>
-	</tr>
-	<tr>
-		<th>Control question: the sum of <?php $this->a; ?> and <?php $this->b; ?></th>
-		<td><input type=text name=sum></td>
-	</tr>
-	<tr>
-		<td colspan=2 align=center><input type=submit value='Go!'></td>
-	</tr>
-	</table>
+	<div class='box'>
+		<div class='box-header'>
+			<h3 class='box-title'>Tag roller</h3>
+			<div class="box-tools pull-right">
+        		<button data-original-title="Collapse" class="btn btn-default btn-sm" data-widget="collapse" data-toggle="tooltip" title=""><i class="fa fa-minus"></i></button>
+        	</div>
+		</div>
+		<div class='box-body'>
+			<p>This special tool allows assigning tags to physical contents (racks <strong>and all contained objects</strong>) of the current ack row.<br>
+			The tag(s) selected below will be appended to already assigned tag(s) of each particular entity.</p>
+			
+			<div class="form-group">
+				<label>Tags</label><br />
+				<?php $this->Tags; ?>
+			</div>
+			
+			<div class="form-group">
+				<label for="sum">Control questions: the sum of <?php $this->a;?> and <?php $this->b;?></label>
+				<input type=text name=sum id=sum class="form-control">
+			</div>
+			
+			<div class="text-center">
+				<button type="submit" class="btn btn-success btn-lg">Submit</button>
+			</div>
+		</div>
+	</div>
 	</form>
 	<?php } else { ?>
 Don't use this page directly, it's supposed <br />
