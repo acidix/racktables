998c0412d4fc8e6ea8662ab7d779c174188209b8
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 15:27:58 +0200

    Made width smaller again.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGRack.tpl.php
+++ wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGRack.tpl.php
@@ -1,6 +1,6 @@
 <?php if (defined("RS_TPL")) {?>
-	<rect x="<?php $this->X?>" y="<?php $this->Y ?>" width="192" height="<?php $this->Height; ?>" fill="none" stroke="black"/>
-	<rect x="<?php echo ($this->_X + 5) ?>" y="<?php echo ($this->_Y + 5) ?>" width="182" height="<?php echo $this->_Height - 10; ?>" fill="#668aff" stroke="black"/>
+	<rect x="<?php $this->X?>" y="<?php $this->Y ?>" width="190" height="<?php $this->Height; ?>" fill="none" stroke="black"/>
+	<rect x="<?php echo ($this->_X + 5) ?>" y="<?php echo ($this->_Y + 5) ?>" width="180" height="<?php echo $this->_Height - 10; ?>" fill="#668aff" stroke="black"/>
 	<svg width="180" height="<?php echo ($this->_Height - 11) ?>" x="<?php echo ($this->_X + 6) ?>" y="<?php echo ($this->_Y + 6) ?>">
 		<defs>
 			<style>
