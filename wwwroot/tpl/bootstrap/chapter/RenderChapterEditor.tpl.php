843231df9ef50c580e2b0554bb6a5956fbf9bd84
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 19:41:47 +0200

    Ported chapter.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/chapter/RenderChapterEditor.tpl.php
+++ wwwroot/tpl/bootstrap/chapter/RenderChapterEditor.tpl.php
@@ -1,63 +1,67 @@
 <?php if (defined("RS_TPL")) {?>
-<br><table class=cooltable border=0 cellpadding=5 cellspacing=0 align=center>
-<tr><th>Origin</th><th>Key</th><th>&nbsp;</th><th>Word</th><th>&nbsp;</th></tr>
-<?php if ($this->is('NewTop')) { ?>
-	<?php $this->getH('PrintOpFormIntro', 'add'); ?>
-	<tr>
-		<td>&nbsp;</td><td>&nbsp;</td>
-		<td>
-			<?php $this->getH('PrintImageHref', array('add', 'Add new', TRUE)); ?>
-		</td>
-		<td class=tdleft><input type=text name=dict_value size=64 tabindex=100></td>
-		<td>
-			<?php $this->getH('PrintImageHref',array('add', 'Add new', TRUE, 101)); ?>
-		</td>
-	</tr>
-	</form>
-<?php } ?>
-<?php while($this->loop('ChapterList')) { ?>
-	<tr class=row_<?php $this->order ?>>
-	<td>
-	<?php if($this->is('lowkey', true)) {
-	    	$this->getH('PrintImageHref', 'computer'); ?>
-		</td>
-		<td class=tdright><?php $this->key ?></td>
-		<td>&nbsp;</td><td><?php $this->value ?></td>
-		<td>&nbsp;</td>
-	<?php } else { ?>
-		<?php $this->getH('OpFormIntro', array('upd', array ('dict_key' => $this->_key))); 
-			  $this->getH('PrintImageHref', 'favorite');?>
-		</td>
-		<td class=tdright><?php $this->key ?></td>
-		<td>
-			<?php if($this->_refcnt) $this->getH('PrintImageHref', array('nodelete', 'referenced ' . $this->_refcnt . ' time(s)')); 
-	    		  else $this->getH('GetOpLink', array(array('op'=>'del', 'dict_key'=>$this->_key), '', 'delete', 'Delete word'))//echo getOpLink (array('op'=>'del', 'dict_key'=>$key), '', 'delete', 'Delete word'); ?>
-		</td>
-		<td class=tdleft><input type=text name=dict_value size=64 value='<?php $this->value ?>'></td><td>
-			<?php $this->getH('printImageHref', array('save', 'Save changes', TRUE)) ?>
-		</td>	
-		</form>
-	<?php } ?>
+<div class="box box-primary">
+	<div class="box-header">
+		<h3 class="box-title">Chapter editor</h3>
+	</div>
+	<div class="box-body no-padding">
+		<table class="table ">
+		<tr><th>Origin</th><th>Key</th><th>Word</th><th>&nbsp;</th></tr>
+		<?php if ($this->is('NewTop')) { ?>
+			<?php $this->getH('PrintOpFormIntro', 'add'); ?>
+			<tr>
+				<td>&nbsp;</td><td>&nbsp;</td>
+				<td><input type=text name=dict_value size=64 tabindex=100 class="form-control"></td>
+				<td>
+					<button class="btn btn-primary" name="submit"><span class="glyphicon glyphicon-plus"></span></button>
+				</td>
+			</tr>
+			</form>
+		<?php } ?>
+		<?php while($this->loop('ChapterList')) { ?>
+		<tr>
+		<?php if($this->is('lowkey', true)) { ?>
+			<td>
+	    		<?php //$this->getH('PrintImageHref', 'computer'); ?>
+	    		<i class="fa fa-fw fa-desktop"></i>
+			</td>
+			<td><?php $this->key ?></td>
+			<td><?php $this->value ?></td>
+			<td>&nbsp;</td>
+		<?php } else { ?>
+			<?php $this->getH('PrintOpFormIntro', array('upd', array ('dict_key' => $this->_key))); ?>
+			<td>
+				<i class="fa fa-fw fa-star"></i>
+			</td>
+			<td><?php $this->key ?></td>
+			<td class=tdleft><input type=text name=dict_value size=64 value='<?php $this->value ?>' class='form-control'></td>
+			<td>
+				<div class="btn-group">
+					<button class="btn btn-success" name="submit"><span class="glyphicon glyphicon-ok"></span></button>
+					<?php if($this->is('refcnt',true)) { ?>
+						<a href="#" class="btn btn-danger" title="referenced <?php $this->refcnt ?> time(s)"><span class="glyphicon glyphicon-remove"></span></a>
+					<?php } else { ?>
+						<?php $this->getH('GetOpLink', array(array('op'=>'del', 'dict_key'=>$this->_key), '', '', 'Delete word', 'btn btn-danger')); ?>
+					<?php } ?>
+				</div>
+			</td>	
+			</form>
+		<?php } ?>
 	</tr>
 <?php } ?>
 <?php if (!$this->is('NewTop')) { ?>
 	<?php $this->getH('PrintOpFormIntro', 'add'); ?>
-	<tr>
-		<td>&nbsp;</td><td>&nbsp;</td>
-		<td>
-			<?php $this->getH('PrintImageHref', array('add', 'Add new', TRUE)); ?>
-		</td>
-		<td class=tdleft><input type=text name=dict_value size=64 tabindex=100></td>
-		<td>
-			<?php $this->getH('PrintImageHref',array('add', 'Add new', TRUE, 101)); ?>
-		</td>
-	</tr>
-	</form>
+			<tr>
+				<td>&nbsp;</td><td>&nbsp;</td>
+				<td><input type=text name=dict_value size=64 tabindex=100 class="form-control"></td>
+				<td>
+					<button class="btn btn-primary" name="submit"><span class="glyphicon glyphicon-plus"></span></button>
+				</td>
+			</tr>
+			</form>
 <?php } ?>
 	  </table>
-
-
-
+	</div>
+</div>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
 to get loaded within the main page. <br />
