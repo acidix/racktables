26e6cde6faab4fb7d73ccac90947fa2ea6e7667f
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 15:05:36 +0200

    Fixing dragndrop.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVG.tpl.php
+++ wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVG.tpl.php
@@ -1,9 +1,7 @@
 <?php if (defined("RS_TPL")) {?>
-	<svg width="<?php $this->OverallWidth; ?>" height="<?php $this->OverallHeight; ?>">
+	<svg width="<?php $this->OverallWidth; ?>" height="<?php $this->OverallHeight; ?>" id="viewport">
 		<script xlink:href="./js/SVGPan.js"/>
-		<g id="viewport">
- 			<?php $this->Content; ?>
- 		</g>
+ 		<?php $this->Content; ?>
  	</svg>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
