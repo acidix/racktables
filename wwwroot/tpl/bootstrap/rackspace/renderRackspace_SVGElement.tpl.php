d0b18b68a213031847980ae08a64a8ae13f322bb
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 14:40:35 +0200

    Fixed link.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGElement.tpl.php
+++ wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGElement.tpl.php
@@ -2,8 +2,8 @@
 <g class="<?php $this->Class ?>">
 	<a xlink:href="<?php $this->Link ?>">
 		<rect x="<?php $this->X ?>" y="<?php $this->Y ?>" width="<?php $this->Width ?>" height="<?php $this->Height ?>" stroke="black" />
+		<text style="font-size: 5" text-anchor="middle" x="<?php echo ($this->_X + ($this->_Width / 2)) ?>" y="<?php echo ($this->_Y + 10) ?>"><?php $this->Name; ?></text>
 	</a>
-	<text style="font-size: 5" text-anchor="middle" x="<?php echo ($this->_X + ($this->_Width / 2)) ?>" y="<?php echo ($this->_Y + 10) ?>"><?php $this->Name; ?></text>
 </g>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
