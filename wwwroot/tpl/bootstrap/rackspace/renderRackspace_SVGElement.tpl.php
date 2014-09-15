54e541a36bc8da2340d8039b4b471560ebf56a95
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 15:19:27 +0200

    Removed Link when no Link exists.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGElement.tpl.php
+++ wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVGElement.tpl.php
@@ -1,9 +1,9 @@
 <?php if (defined("RS_TPL")) {?>
 <g class="<?php $this->Class ?>">
-	<a xlink:href="<?php $this->Link ?>">
+	<?php if (!$this->is('Link','#')) { ?><a xlink:href="<?php $this->Link ?>"><?php } ?>
 		<rect x="<?php $this->X ?>" y="<?php $this->Y ?>" width="<?php $this->Width ?>" height="<?php $this->Height ?>" stroke="black" />
 		<text style="font-size: 9px;" text-anchor="middle" x="<?php echo ($this->_X + ($this->_Width / 2)) ?>" y="<?php echo ($this->_Y + 8) ?>"><?php $this->Name; ?></text>
-	</a>
+	<?php if (!$this->is('Link','#')) { ?></a><?php } ?>
 </g>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
