6dadcb494f55a6921484d73c3aca43657826cfad
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-15 14:52:44 +0200

    Added JS to make drag and zoom possible.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVG.tpl.php
+++ wwwroot/tpl/bootstrap/rackspace/renderRackspace_SVG.tpl.php
@@ -1,6 +1,9 @@
 <?php if (defined("RS_TPL")) {?>
 	<svg width="<?php $this->OverallWidth; ?>" height="<?php $this->OverallHeight; ?>">
- 		<?php $this->Content; ?>
+		<script xlink:href="SVGPan.js"/>
+		<g id="viewport">
+ 			<?php $this->Content; ?>
+ 		</g>
  	</svg>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
