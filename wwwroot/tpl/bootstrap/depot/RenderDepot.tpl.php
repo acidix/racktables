841fc51648d26f7a370ea2f458a8bfc2c829c162
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-16 13:56:53 +0200

    Fixed typo?
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/depot/RenderDepot.tpl.php
+++ wwwroot/tpl/bootstrap/depot/RenderDepot.tpl.php
@@ -1,10 +1,4 @@
-<?php if (defined("RS_TPL")) { 
-	$this->addJS(
-		'$(document).ready( function () {' .
-    	'$("#object_table").dataTable(); ' .
-		'} );',
-	true); 
-?>
+<?php if (defined("RS_TPL")) { ?>
 <div class='row'>
 	<div class='col-md-10'>
 		<div class="box box-primary">
@@ -12,7 +6,7 @@
 				<h3 class="box-title">Objects</h3>
 			</div>
 			<div class="box-body">
-				<table id="object_table" class="display">
+				<table id="object_table" class="table table-bordered table-striped">
 					<thead>
 						<tr><th>Common name</th><th>Visible label</th><th>Asset tag</th><th>Row/Rack or Container</th></tr>
 					</thead>
@@ -46,6 +40,11 @@
 		<?php $this->CellFilterPortlet; ?>
 	</div>
 </div>
+<script type="text/javascript">
+     $(function() {
+          $("#object_table").dataTable();
+     });
+</script>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
 to get loaded within the main page. <br />
