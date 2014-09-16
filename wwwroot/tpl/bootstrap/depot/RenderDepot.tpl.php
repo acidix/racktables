4411e7a505b15fd3bac69772bf65de02a4ea9e89
Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>
2014-09-16 14:00:08 +0200

    Fixed path.
    Signed-off-by: Alexander Kastius <alexander.kastius@student.hpi.uni-potsdam.de>

--- wwwroot/tpl/bootstrap/depot/RenderDepot.tpl.php
+++ wwwroot/tpl/bootstrap/depot/RenderDepot.tpl.php
@@ -42,8 +42,8 @@
 </div>
 
 <!-- DATA TABES SCRIPT -->
-<script src="../../js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="../../js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="./js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+<script src="./js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
 
 <script type="text/javascript">
      $(function() {
