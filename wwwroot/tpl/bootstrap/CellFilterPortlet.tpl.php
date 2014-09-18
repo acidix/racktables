aa2af4a25d66c26ce5cf701316e6c8c9cc984a0f
Jakob Frick <jakob@aussenwasser.de>
2014-09-19 00:06:14 +0200

    Changed cell filters

--- wwwroot/tpl/bootstrap/CellFilterPortlet.tpl.php
+++ wwwroot/tpl/bootstrap/CellFilterPortlet.tpl.php
@@ -1,47 +1,53 @@
-<?php if (defined("RS_TPL")) {?>
+-<?php if (defined("RS_TPL")) {?>
 	<?php 	$this->addRequirement("Header","HeaderJsInclude",array("path"=>"js/tag-cb.js"));
-		 	$this->addRequirement("Header","HeaderJsInline",array("code"=>"tag_cb.enableNegation()")); ?>
-	<div class=portlet><h2><?php $this->get("PortletTitle"); ?></h2>
+		 	$this->addRequirement("Header","HeaderJsInline",array("code"=>"tag_cb.enableNegation()")); 
+		 	$this->addRequirement("Header","HeaderCssInclude", array("path" => "css/racktables_bootstrap.css"));?>	
+    
+	<button class="btn btn-default" onmousedown="$('#cellfilterport').toggle('blind')">Show tag filter</button>
+    <div class="panel panel-default overlay" id="cellfilterport">
+    	<div class="panel-heading"><h2><?php $this->get("PortletTitle"); ?></h2></div>
+		<div class="panel-body">
 		<form method=get>
 			<table border=0 align=center cellspacing=0 class="tagtree">
-				<?php $this->TableContent; ?>
-				<?php if ($this->is('EnableSubmitOnClick')) { ?>
-					<?php $this->addRequirement("Header","HeaderJsInline",array("code"=>"tag_cb.enableSubmitOnClick()")); ?>
-				<?php } ?> 
-				<tr>
-					<td class="tdleft">
-						<input type=hidden name=page value=<?php $this->PageNo; ?>>
-						<input type=hidden name=tab value=<?php $this->TabNo; ?>>
-						<?php $this->HiddenParams; ?>
-						<?php if ($this->is("EnableApply",true)) { ?>
-							<input class="icon" type="image" border="0" title="set filter" src="?module=chrome&uri=pix/pgadmin3-viewfiltereddata.png" name="submit"></input>
-						<?php } ?>
-						<?php if ($this->is("EnableApply",false)) { ?>
-							<img src="pix/pgadmin3-viewfiltereddata-grayscale.png" width=32 height=32 border=0>
-						<?php }?>
-						<?php $this->Textify; ?>
-					</td>
-					<td class=tdright>
-						<?php if ($this->is("EnableReset",false)) { ?>
-							<img src="pix/pgadmin3-viewdata-grayscale.png" width=32 height=32 border=0>
-						<?php } ?>
-						<?php if ($this->is("EnableReset",true)) { ?>
-							<form method=get>
-								<input type=hidden name=page value=<?php $this->PageNo; ?>>
-								<input type=hidden name=tab value=<?php $this->TabNo; ?>>
-								<input type=hidden name='cft[]' value=''>
-								<input type=hidden name='cfp[]' value=''>
-								<input type=hidden name='nft[]' value=''>
-								<input type=hidden name='nfp[]' value=''>
-								<input type=hidden name='cfe' value=''>
-								<?php $this->HiddenParamsReset; ?>
-								<input class="icon" type="image" border="0" title="reset filter" src="?module=chrome&uri=pix/pgadmin3-viewdata.png" name="submit"></input>
-							</form>
-						<?php } ?>
-					</td>
-				</tr>
+			<?php $this->TableContent; ?>
+			<?php if ($this->is('EnableSubmitOnClick')) { ?>
+				<?php $this->addRequirement("Header","HeaderJsInline",array("code"=>"tag_cb.enableSubmitOnClick()")); ?>
+			<?php } ?> 
+			<tr>
+				<td class="tdleft">
+					<input type=hidden name=page value=<?php $this->PageNo; ?>>
+					<input type=hidden name=tab value=<?php $this->TabNo; ?>>
+					<?php $this->HiddenParams; ?>
+					<?php if ($this->is("EnableApply",true)) { ?>
+						<input class="icon" type="image" border="0" title="set filter" src="?module=chrome&uri=pix/pgadmin3-viewfiltereddata.png" name="submit"></input>
+					<?php } ?>
+					<?php if ($this->is("EnableApply",false)) { ?>
+						<img src="pix/pgadmin3-viewfiltereddata-grayscale.png" width=32 height=32 border=0>
+					<?php }?>
+					<?php $this->Textify; ?>
+				</td>
+				<td class=tdright>
+					<?php if ($this->is("EnableReset",false)) { ?>
+						<img src="pix/pgadmin3-viewdata-grayscale.png" width=32 height=32 border=0>
+					<?php } ?>
+					<?php if ($this->is("EnableReset",true)) { ?>
+						<form method=get>
+							<input type=hidden name=page value=<?php $this->PageNo; ?>>
+							<input type=hidden name=tab value=<?php $this->TabNo; ?>>
+							<input type=hidden name='cft[]' value=''>
+							<input type=hidden name='cfp[]' value=''>
+							<input type=hidden name='nft[]' value=''>
+							<input type=hidden name='nfp[]' value=''>
+							<input type=hidden name='cfe' value=''>
+							<?php $this->HiddenParamsReset; ?>
+							<input class="icon" type="image" border="0" title="reset filter" src="?module=chrome&uri=pix/pgadmin3-viewdata.png" name="submit"></input>
+						</form>
+					<?php } ?>
+				</td>
+			</tr>
 			</table>
 		</form>
+		</div>
 	</div>
 <?php } else { ?>
 Don't use this page directly, it's supposed <br />
