5eb3e0759906b25cccfeefec0dc8962f67932a33
Jakob Frick <jakob@aussenwasser.de>
2014-09-26 12:35:36 +0200

    Added dynamic input rows for add object

--- wwwroot/tpl/bootstrap/index.tpl.php
+++ wwwroot/tpl/bootstrap/index.tpl.php
@@ -32,8 +32,8 @@
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
-		
+        <![endif]-->	
+
 		<!-- jQuery 2.0.2 -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
         <!-- Bootstrap -->
@@ -42,7 +42,7 @@
         <script src="./js/AdminLTE/app.js" type="text/javascript"></script>
         <!-- AdminLTE for demo purposes -->
         <script src="./js/AdminLTE/demo.js" type="text/javascript"></script>
-        
+       
 		<?php $this->Header ?>
     </head>
     <body class="skin-blue">
@@ -153,15 +153,11 @@
                 <div class="pulse_ring"></div>
             </aside>
 
-            <aside class="left-side sidebar-offcanvas portletbar" onmouseout="$('#portletsidebar').hide('blind')" style="min-height: 542px; display: none;" id="portletsidebar">
+            <aside class="left-side sidebar-offcanvas portletbar" onmouseleave="$('#portletsidebar').hide('blind')" style="min-height: 542px; display: none;" id="portletsidebar">
                 <!-- sidebar: style can be found in sidebar.less -->
-                <section class="sidebar" >
-                    
+                <section class="sidebar">
                     <ul class="sidebar-menu"><li><strong>Tabs:</strong></li><?php $this->Tabs; ?></ul>
-                    <!-- search form -->
-                    
-                    <!-- /.search form -->
-                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                   <!-- sidebar menu: : style can be found in sidebar.less -->
                 </section>   
             </aside>
 
